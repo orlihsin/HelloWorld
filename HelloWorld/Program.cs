fc253e17d9081d4d507d69f9ddac3976ad7ef6af
peter <peter@PERER1>
2021-10-12 20:19:13 +0800

    第一個程式

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World~歡迎學習 C#");
+            Console.ReadKey();
+        }
+    }
+}
