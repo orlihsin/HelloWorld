12204dd753e731b63e6dc93da8f623d701ee8945
peter <peter@PERER1>
2021-10-16 20:25:43 +0800

    Lable與點擊次數記錄程式

---
+++ MouseClick/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MouseClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timeslabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(92, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "按我";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(92, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "重設";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timeslabel
+            // 
+            this.timeslabel.AutoSize = true;
+            this.timeslabel.Font = new System.Drawing.Font("Microsoft JhengHei UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timeslabel.Location = new System.Drawing.Point(50, 39);
+            this.timeslabel.Name = "timeslabel";
+            this.timeslabel.Size = new System.Drawing.Size(211, 35);
+            this.timeslabel.TabIndex = 2;
+            this.timeslabel.Text = "你已經按了...0下";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 272);
+            this.Controls.Add(this.timeslabel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "按鈕點擊次數記錄程式";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label timeslabel;
+    }
+}
+
--- MouseClick/Form1.cs
+++ MouseClick/Form1.cs
@@ -8,36 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Demo.Form1
+namespace MouseClick
 {
     public partial class Form1 : Form
     {
+        int times = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click (object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            string str = "我是小歆";
-            string str2 = "哈哈";
-
-            int number = 321;
-
-            string str3 = str + str2 + number;
-
-            str += str2;
-
-
-           MessageBox.Show(str);
-
-            
-               
-           
+            times++;
+            timeslabel.Text ="你已經點了..." + times+ "下";
         }
 
-        
-
-        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            times = 0;
+            timeslabel.Text = "你已經點了..." + times+ "下";
+        }
     }
 }
