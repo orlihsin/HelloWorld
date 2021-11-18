eeb2c4e491d684471c6989b4628f22a3e2e8972d
peter <peter@PERER1>
2021-11-18 22:04:43 +0800

    Gomoku(動態建立棋子)

---
+++ Gomoku-Demo/Piece.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Gomoku_Demo
+{
+    abstract class Piece : PictureBox
+    {
+     public Piece (int x, int y)
+        {
+            this.BackColor = Color.Transparent;
+            this.Location = new Point(x, y);
+            this.Size = new Size(50, 50);
+        }
+    }
+}
